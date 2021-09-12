7446b8f44a04b5624c9aa6e5842e628578a3bc24
Oreoluwa-IVT <oreoluwaosewe.oluwafemi@stu.cu.edu.ng>
2021-09-12 19:15:56 +0100

    now

--- Flight Booking App.py
+++ Flight Booking App.py
@@ -6,13 +6,15 @@ class Restaurant(Frame):
         self.createwidget()
         
     def createwidget(self):
+        self.lbl=Label(self,text="Enter Username")
+        self.lbl.grid(row=1,column=0, sticky=W)
         self.pw_ent=Entry(self)
-        self.pw_ent.grid(row=1,column=0,sticky=W)
+        self.pw_ent.grid(row=2,column=0,sticky=W)
         self.submit_bttn=Button(self,text="Submit",command=self.reveal) #command here simply triggers the event handler
-        self.submit_bttn.grid(row=2,column=0, sticky=W)
+        self.submit_bttn.grid(row=3,column=0, sticky=W)
         self.secret_txt=Text(self,width=35,height=5,wrap=WORD) #wrap WORD here simply means that once you get the end of the line , 
         #the next word you type goes to the next line
-        self.secret_txt.grid(row=3,column=0,columnspan=2,sticky=W)
+        self.secret_txt.grid(row=9,column=0,columnspan=2,sticky=W)
         
     def reveal(self):
         """Display message based on password"""
