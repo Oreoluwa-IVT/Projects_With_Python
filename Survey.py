1e577810e0bb6212ce3d3928d1bebf8e99ed2034
Oreoluwa-IVT <oreoluwaosewe.oluwafemi@stu.cu.edu.ng>
2021-09-12 20:41:47 +0100

    added

---
+++ Survey.py
@@ -0,0 +1,20 @@
+from tkinter import *
+class survey(Frame):
+    def __init__(self,master):
+        super(survey,self).__init__(master)
+        self.grid()
+        self.createsurvey()
+    
+    def createsurvey(self):
+        self.lbl=Label(self,text="Please Answer the Survey Appropriately, Thanks you").grid(row=0,column=0)
+        self.q1=Label(self,text="Your Full Name").grid(row=0,column=0)
+        self.a1=Entry(self).grid(row=1,column=0)
+
+
+
+
+root=Tk()
+root.geometry=("400X400")
+root.title("Survey App")
+main=survey(root)
+main.mainloop()
