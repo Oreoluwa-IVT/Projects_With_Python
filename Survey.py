137b938273cc58de7588f78f93595c79a24ad036
Oreoluwa-IVT <oreoluwaosewe.oluwafemi@stu.cu.edu.ng>
2021-09-13 00:25:52 +0100

    added

--- Survey.py
+++ Survey.py
@@ -9,12 +9,18 @@ class survey(Frame):
         self.lbl=Label(self,text="Please Answer the Survey Appropriately, Thanks you").grid(row=0,column=0)
         self.q1=Label(self,text="Your Full Name").grid(row=1,column=0, sticky=W)
         self.a1=Entry(self).grid(row=2,column=0, sticky=W)
-
+        self.q2=Label(self,text="Email Address").grid(row=3,column=0,sticky=W)
+        self.a2=Entry(self).grid(row=4,column=0,sticky=W)
+        self.q3=Label(self,text="Residence").grid(row=5,column=0,sticky=W)
+        self.a3=Entry(self).grid(row=6,column=0,sticky=W)
+        
+    
+    
 
 
 
 root=Tk()
-root.geometry=("400X400")
+root.geometry=("900X900")
 root.title("Survey App")
 main=survey(root)
 main.mainloop()
