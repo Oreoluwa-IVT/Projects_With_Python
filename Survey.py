345b96a6a83e434305ae7b2d5dbdfb500238b553
Oreoluwa-IVT <oreoluwaosewe.oluwafemi@stu.cu.edu.ng>
2021-09-13 01:57:37 +0100

    get

--- Survey.py
+++ Survey.py
@@ -14,20 +14,32 @@ class survey(Frame):
         self.q3=Label(self,text="Residence").grid(row=5,column=0,sticky=W)
         self.a3=Entry(self).grid(row=6,column=0,sticky=W)
         self.q4=Label(self,text="Gender").grid(row=7,column=0,sticky=W)
+        self.fav=StringVar()
+        self.fav.set(None)
+        Radiobutton(self,text="Male",variable=self.fav,value="Male",command=self.getvalue).grid(row=8,column=0,sticky=W)
+        Radiobutton(self,text="Female",variable=self.fav,value="Female",command=self.getvalue).grid(row=9,column=0,sticky=W)
+        
+        
         self.checkm=BooleanVar()
         self.checkf=BooleanVar()
-        Checkbutton(self,text="Male",variable=self.checkm,command=self.male).grid(row=8,column=0,sticky=W)
-        Checkbutton(self,text="Female",variable=self.checkf,command=self.female).grid(row=8,column=0,sticky=W)
+        Checkbutton(self,text="Male",variable=self.checkm,command=self.male).grid(row=15,column=0,sticky=W)
+        Checkbutton(self,text="Female",variable=self.checkf,command=self.female).grid(row=16,column=0,sticky=W)
+        self.q5=Label(self,text="Your Religion").grid(row=17,column=0,sticky=W)
+        self.a4=Entry(self).grid(row=18,column=0,sticky=W)
         
 
+
+
+    def male(self):
+        print("Male")
         
+    def female(self):
+        print("Female")
         
         
     
-    
-
-
-
+        
+        
 root=Tk()
 root.geometry=("900X900")
 root.title("Survey App")
