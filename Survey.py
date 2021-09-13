eca3e2c9bd20876a38d40f01ef48eb6060663b08
Oreoluwa-IVT <oreoluwaosewe.oluwafemi@stu.cu.edu.ng>
2021-09-13 02:07:19 +0100

    added

--- Survey.py
+++ Survey.py
@@ -18,15 +18,21 @@ class survey(Frame):
         self.fav.set(None)
         Radiobutton(self,text="Male",variable=self.fav,value="Male",command=self.getvalue).grid(row=8,column=0,sticky=W)
         Radiobutton(self,text="Female",variable=self.fav,value="Female",command=self.getvalue).grid(row=9,column=0,sticky=W)
-        
-        
         self.checkm=BooleanVar()
         self.checkf=BooleanVar()
-        Checkbutton(self,text="Male",variable=self.checkm,command=self.male).grid(row=15,column=0,sticky=W)
+        self.q5=Label(self,text="Courses").grid(row=7,column=0,sticky=W)
+        Checkbutton(self,text="",variable=self.checkm,command=self.male).grid(row=15,column=0,sticky=W)
         Checkbutton(self,text="Female",variable=self.checkf,command=self.female).grid(row=16,column=0,sticky=W)
-        self.q5=Label(self,text="Your Religion").grid(row=17,column=0,sticky=W)
+        self.q6=Label(self,text="Your Religion").grid(row=17,column=0,sticky=W)
         self.a4=Entry(self).grid(row=18,column=0,sticky=W)
         
+        
+    def getvalue(self):
+        self.fav.get()
+    
+      
+    
+
 
 
 
